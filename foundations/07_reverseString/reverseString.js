4dfc42c91be0dc342c22f1be8bba074e66b63d2b
laplage96 <fritzfunyui@gmail.com>
2026-07-06 23:31:00 +0100

    feat: implement reverseString logic using backwards loop

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedResult = "";
+    for (let i = string.length - 1; i >= 0; i--){
+        reversedResult += string[i]
+    }
+
+    return reversedResult;
 
 };
 
