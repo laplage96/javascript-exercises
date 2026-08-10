90e93877c177ae08eefe3f3725e29cee0e5f1c71
laplage96 <fritzfunyui@gmail.com>
2026-08-10 17:56:57 +0100

    Complete palindromes and fibonacci exercises

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+//reverse the clean string
+const reversedStr = cleanStr.split('').reverse().join('');
+
+//Compare the cleaned string with its reversed version
+return cleanStr === reversedStr;
 };
 
 // Do not edit below this line
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+const count = Number(n);
 
+//handle edge cases for zero or negative numbers
+if (count < 0) return "OOPS";
+if (count === 0) return 0;
+if (count === 1 || count === 2) return 1;
+
+let prev = 1;
+let current = 1;
+
+for (let i = 3; i <= count ; i++) {
+    let next = prev + current;
+    prev = current;
+    current = next;
+
+}
+return current;
 };
 
 // Do not edit below this line
